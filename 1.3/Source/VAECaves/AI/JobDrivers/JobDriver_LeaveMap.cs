06a0d927dd2c2a594ae08e578b5780f41d2bb6bd
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-07-29 15:55:37 +0200

    v0.1

---
+++ 1.3/Source/VAECaves/AI/JobDrivers/JobDriver_LeaveMap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using RimWorld.Planet;
+using System.Linq;
+
+namespace VAECaves
+{
+	public class JobDriver_LeaveMap : JobDriver
+	{
+		public override bool TryMakePreToilReservations(bool errorOnFailed)
+		{
+			pawn.Map.pawnDestinationReservationManager.Reserve(pawn, job, job.targetA.Cell);
+			return true;
+		}
+
+		protected override IEnumerable<Toil> MakeNewToils()
+		{
+			Toil toil = Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.OnCell);
+			toil.AddPreTickAction(delegate
+			{
+				if (job.exitMapOnArrival && pawn.Map.exitMapGrid.IsExitCell(pawn.Position))
+				{
+					pawn.ExitMap(allowedToJoinOrCreateCaravan: true, CellRect.WholeMap(base.Map).GetClosestEdge(pawn.Position));
+				}
+			});
+			toil.FailOn(() => job.failIfCantJoinOrCreateCaravan && !CaravanExitMapUtility.CanExitMapAndJoinOrCreateCaravanNow(pawn));
+			yield return toil;
+			Toil toil2 = new Toil();
+			toil2.initAction = delegate
+			{
+				if (pawn.mindState != null && pawn.mindState.forcedGotoPosition == base.TargetA.Cell)
+				{
+					pawn.mindState.forcedGotoPosition = IntVec3.Invalid;
+				}
+				if (job.exitMapOnArrival && (pawn.Position.OnEdge(pawn.Map) || pawn.Map.exitMapGrid.IsExitCell(pawn.Position)))
+				{
+					pawn.ExitMap(allowedToJoinOrCreateCaravan: true, CellRect.WholeMap(base.Map).GetClosestEdge(pawn.Position));
+				}
+			};
+			toil2.defaultCompleteMode = ToilCompleteMode.Instant;
+			yield return toil2;
+		}
+
+		
+
+	}
+}
